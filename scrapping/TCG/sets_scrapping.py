6a91c99a7c44781c345d30339c73abe5a8c54028
rvegarodz <r.vega27@outlook.com>
2023-08-22 15:13:53 -0400

    adding files to scrap tcg sets

---
+++ scrapping/TCG/sets_scrapping.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+'''This document contains web scrapping process for TCG Sets'''
+
+
+import requests
+import json
+from bs4 import BeautifulSoup
+
+
+def get_sets():
+    '''Function that web scrap and generate json file'''
+    tcg_sets = {}
+    uri = 'https://www.pocketmonsters.net/tcg'
+    response = requests.get(uri)
+    soup = BeautifulSoup(response.text, "html.parser")
+    lst = soup.find_all('a')
+
+    for i in range(37, 236):
+        set_name = lst[i].get_text()
+        set_img = lst[i].get('href')
+        tcg_sets[set_name] = set_img
+
+    with open('tcg_sets.json', 'w') as f:
+        json.dump(tcg_sets, f)
+
+# Use function:
+# get_sets()
