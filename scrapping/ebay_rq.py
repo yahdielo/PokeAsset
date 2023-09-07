69d9def1830d492dca46df06601fba269cd43e45
rvegarodz <r.vega27@outlook.com>
2023-09-07 16:10:31 -0400

    function that return url for ebay request

---
+++ scrapping/ebay_rq.py
@@ -0,0 +1,27 @@
+import requests
+
+def ebay_url(poke_name, card_type, foil_type, psa_num):
+    '''Function that return a url for the ebay request'''
+    poke_search = poke_name + "+"
+    card_type_search = card_type + "+"
+    foil_search = foil_type + "+"
+    psa_search = "psa+" + str(psa_num)
+    ebay_search = poke_search + card_type_search + foil_search + psa_search
+    url = f"https://www.ebay.com/sch/i.html?_from=R40&_sacat=0&LH_TitleDesc=0&_nkw={ebay_search}&rt=nc&LH_Sold=1&LH_Complete=1"
+    print(ebay_search)
+    print(url)
+    return (url)
+
+# Example of how to use the function:
+# poke_name should be a string with the pokemon name
+poke_name = "pikachu"
+# card_type should be a string with the type of ultra rare and secret pokémon cards
+# example: vmax, gx, etc.
+card_type = "vmax"
+# foil_type should be a string with the foil type of the card
+foil_type = "rainbow"
+# psa should be a int (can be string) with the grade of psa
+psa = 10
+
+
+uri = ebay_url(poke_name, card_type, foil_type, psa)
